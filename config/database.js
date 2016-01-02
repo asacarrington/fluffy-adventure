8990596cae7c953d89ae7fe8a3d691aaf5b8403c
Asa Carrington <asacarrington@Asas-MBP.home>
2016-01-02 20:19:54 +0000

    connection string change

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
  * @author Asa Carrington
  * Establishes mongoose connection properties.
  */
-var uri = 'mongodb://localhost:27017/fluffy-adventure0db';
+var uri = 'mongodb://localhost:27017/fluffy-adventure-db';
 var mongoose = require('mongoose');
 mongoose.connect(uri);
 
